555e4e36d40c8f7e292d2fb8c52066aefe283f2d
eishimoto <eishimoto@outlook.com>
2022-05-28 17:50:15 -0300

    new model and animation
    new model and animation

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,66 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField] private float m_MoveSpeed = 3;
-    [SerializeField] private float m_time = 3;
-    [SerializeField] private Vector3 m_Direction1;
-    [SerializeField] private Vector3 m_Direction2;
-    [SerializeField] private Vector3 m_Direction3;
-    [SerializeField] private Vector3 m_Direction4;
-    DetectionCollider DetectionCollider;
-    void Start()
-    {
-        StartCoroutine(WalkFoward());
-    }
-    IEnumerator WalkFoward()
-    {
-        float time = Time.time + m_time;
-        while (time > Time.time)
-        {
-            Vector3 move = m_Direction1 * m_MoveSpeed * Time.deltaTime;
-            transform.Translate(move);
-           
-            yield return null;
-        }
-        StartCoroutine(WalkRight());
-    }
-    IEnumerator WalkRight()
-    {
-        float time = Time.time + m_time;
-        while (time > Time.time)
-        {
-            Vector3 move = m_Direction2 * m_MoveSpeed * Time.deltaTime;
-            transform.Translate(move);
-            
-            yield return null;
-        }
-        StartCoroutine(WalkDown());
-    }
-    IEnumerator WalkDown()
-    {
-        float time = Time.time + m_time;
-        while (time > Time.time)
-        {
-            Vector3 move = m_Direction3 * m_MoveSpeed * Time.deltaTime;
-            transform.Translate(move);
-   
-            yield return null;
-        }
-        StartCoroutine(WalkLeft());
-    }
-    IEnumerator WalkLeft()
-    {
-        float time = Time.time + m_time;
-        while (time > Time.time)
-        {
-            Vector3 move = m_Direction4 * m_MoveSpeed * Time.deltaTime;
-            transform.Translate(move);
+    [SerializeField] private Transform player;
+    private NavMeshAgent meshAgent;
 
-            yield return null;
+    private void Start()
+    {
+        meshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position,player.position) > 10)
+        {
+            meshAgent.speed = 10;
         }
-        StartCoroutine(WalkFoward());
+        else
+        {
+            meshAgent.speed = 4;
+        }
     }
 }
